3852a2e801ab96df52774b307dd0009442d8ef0c
Hafiz-Farook <Hafiz.Farook@student.uts.edu.au>
2025-11-17 13:24:11 +1100

    Update 05_query.sql

--- queries/05_query.sql
+++ queries/05_query.sql
@@ -1,4 +1,4 @@
-WITH price_changes AS (
+WITH price_history AS (
     SELECT
         p.product_id,
         p.product_name,
@@ -8,27 +8,23 @@ WITH price_changes AS (
     JOIN order_details od
         ON od.product_id = p.product_id
     GROUP BY
-        p.product_id,
-        p.product_name,
-        p.unit_price
+        p.product_id, p.product_name, p.unit_price
 ),
-with_increase AS (
+price_changes AS (
     SELECT
-        product_id,
         product_name,
-        current_unit_price,
-        initial_unit_price,
-        (current_unit_price - initial_unit_price)
-            / initial_unit_price * 100.0 AS pct_increase
-    FROM price_changes
+        ROUND(current_unit_price::numeric, 2) AS current_price,
+        ROUND(initial_unit_price::numeric, 2) AS initial_price,
+        ((current_unit_price - initial_unit_price) / initial_unit_price * 100) AS pct_increase
+    FROM price_history
     WHERE current_unit_price > initial_unit_price
 )
 SELECT
     product_name,
-    ROUND(current_unit_price::numeric, 2) AS current_unit_price,
-    ROUND(initial_unit_price::numeric, 2) AS initial_unit_price,
+    current_price,
+    initial_price,
     ROUND(pct_increase)::int AS percentage_increase
-FROM with_increase
+FROM price_changes
 WHERE
     pct_increase < 20
     OR pct_increase > 30
